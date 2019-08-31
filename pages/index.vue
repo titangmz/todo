a4ae2967ec4feaa4674640aaa356a62e4d5d6f0a
Pedram Parsa <titangmz@gmail.com>
2019-08-31 17:45:04 +0430

    Updated

--- pages/index.vue
+++ pages/index.vue
@@ -10,42 +10,17 @@
       md6
     >
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
+
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Task
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
+          
           <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+    
+          
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
           </div>
@@ -80,13 +55,8 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   }
 }
 </script>
